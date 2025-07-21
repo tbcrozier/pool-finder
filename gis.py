445ec5eb1f0f00925569e3e1d0f79f66ac0b4072
Blake Crozier <crozitb0@gmail.com>
2025-07-20 21:16:17 -0500

    Pool Finder api testing using arcgis endpoint

---
+++ gis.py
@@ -0,0 +1,18 @@
+import requests
+
+url = "https://maps.nashville.gov/arcgis/rest/services/Cadastral/Parcels/MapServer/0/query"
+
+data = {
+    "where": "PropZip='37205'",
+    "outFields": "*",  
+    "returnGeometry": "true",
+    "returnZ": "false",
+    "returnM": "false",
+    "f": "json"
+}
+
+r = requests.post(url, data=data)
+parcel_data = r.json()
+
+# Preview one parcel
+print(parcel_data["features"][0])
